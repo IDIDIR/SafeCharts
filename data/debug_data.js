8f59b94906b442d6c01b59c0e8f2120a644438ef
DDR <dmitriycrtr@outlook.com>
2019-03-19 18:18:49 +0600

    adapted origin & beautified axis

--- data/debug_data.js
+++ data/debug_data.js
@@ -1,4 +1,4 @@
-[
+var bigdata = [
 	{
 		"columns": [
 			[
--- js/safeCore.js
+++ js/safeCore.js
@@ -1,48 +1,76 @@
-/*!
-	* SafeCharts v0.0.1
-	* Copyright 2019 The AICC inc.
-  * Very speed, efficiency and the size of the app.
-  */
+/*
+  * SafeCharts v0.0.1
+   * Copyright 2019 The AICC inc.
+    * Very speed, efficiency and the size of the app.
+     * * * * * * * * * * * * * * * * * * * * * * * * */
 
 "use strict"
 
 const generalConst = 'bazat'
+const DEFAULTS = {
+	xAxis: {
+		offset: 30,
+		padding: 10,
+		// lines: dynamic
+	},
+	yAxis: {
+		offset: 30,
+		padding: 0,
+		labels: 6,
+		lines: 6,
+	},
+	lineJoin: 'round',
+	rowHeight: 60
+}
+
+var drawAxisLables = function() {
+	let rowCount = DEFAULTS.yAxis.labels
+	let colCount = 6 // calc
+	let rowHeight = DEFAULTS.rowHeight
+	ctx.font = "1em Candara"
+	ctx.fillStyle = '#ADADADAD'
+
+	ctx.textAlign = 'left'
+	ctx.translate(0, -DEFAULTS.yAxis.offset)
+	ctx.beginPath()
+	// yAxis labels (static)
+	for (var i = 0; i < rowCount; i++) {
+		ctx.fillText(rowHeight * i, 0, CANVAS.height - rowHeight * i)
+	}
+	ctx.stroke()
+
+	ctx.textAlign = 'center'
+	ctx.translate(DEFAULTS.xAxis.offset, DEFAULTS.yAxis.offset)
+	ctx.beginPath()
+	// xAxis labels (dynamic)
+	for (var i = 1; i <= colCount; i++) {
+		ctx.fillText(`Mar ${i}`, rowHeight * i, CANVAS.height - DEFAULTS.xAxis.padding)
+	}
+	ctx.stroke()
+	
+	ctx.translate(0, -DEFAULTS.yAxis.offset)
+}
 
 var drawPlotLines = function() {
-	let localConst = generalConst.length + 1
-	let lineHeight = 10 * localConst
+	let rowCount = DEFAULTS.yAxis.labels
+	let colCount = 6 // calc
+	let rowHeight = DEFAULTS.rowHeight
 	ctx.lineWidth = 0.5
 	ctx.strokeStyle = '#CFCFCFCF'
 	ctx.beginPath()
-	for (var i = 1; i <= localConst; i++) {
-		ctx.moveTo(0, lineHeight * i)
-		ctx.lineTo(400, lineHeight * i)
+	// xAxis labels
+	for (var i = 0; i < colCount; i++) {
+		ctx.moveTo(0, CANVAS.height - rowHeight * i)								// 1
+		ctx.lineTo(CANVAS.height, CANVAS.height - rowHeight * i)		// 2
 	}
 	ctx.stroke()
 	ctx.beginPath()
 	ctx.strokeStyle = '#E9E9E9E9'
-	for (var i = 1; i <= localConst; i++) {
-		ctx.moveTo(lineHeight * i, 0)
-		ctx.lineTo(lineHeight * i, 400)
-	}
-	ctx.stroke()
-}
-
-var drawAxisLables = function() {
-	let localConst = generalConst.length + 1
-	let lineHeight = 10 * localConst
-	let yAxisPadding = { bottom: 5}
-	let xAxisPadding = { top: 20}
-	ctx.font = "1em Candara"
-	ctx.textAlign = 'left'
-	ctx.fillStyle = '#ADADADAD'
-	ctx.beginPath()
-	for (var i = 1; i <= localConst; i++) {
-		ctx.fillText(lineHeight * i, 0, lineHeight * i - yAxisPadding.bottom)
-	}
-	ctx.textAlign = 'center'
-	for (var i = 1; i <= localConst; i++) {
-		ctx.fillText(`Mar ${i}`, lineHeight * i, 400 - xAxisPadding.top)
+	// yAxis labels
+	for (var i = 0; i <= rowCount; i++) {
+		// because need to think like the bottom left origin
+		ctx.moveTo(rowHeight * i, CANVAS.height)										// 2
+		ctx.lineTo(rowHeight * i, 0)																// 1
 	}
 	ctx.stroke()
 }
@@ -50,23 +78,49 @@ var drawAxisLables = function() {
 var drawTempChart = function() {
 	ctx.lineWidth = 2.5
 	ctx.strokeStyle = '#C99898'
+	ctx.lineJoin = DEFAULTS.lineJoin
 	ctx.beginPath()
-	ctx.moveTo(0, 80)
-	ctx.lineTo(60, 340)
-	ctx.lineTo(120, 210)
-	ctx.lineTo(180, 290)
-	ctx.lineTo(240, 200)
-	ctx.lineTo(300, 40)
-	ctx.lineTo(360, 260)
-	ctx.lineTo(400, 190)
+	ctx.moveTo(0, 	rev(80))
+	ctx.lineTo(60, 	rev(340))
+	ctx.lineTo(120, rev(210))
+	ctx.lineTo(180, rev(290))
+	ctx.lineTo(240, rev(200))
+	ctx.lineTo(300, rev(40))
+	ctx.lineTo(360, rev(260))
+	ctx.lineTo(400, rev(190))
 	ctx.stroke()
 }
 
-var ctx = document.getElementById("safeChart").getContext("2d")
+var rev =(y)=> CANVAS.height-y
 
-// draw some lines
-drawPlotLines()
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+const CANVAS = document.getElementById("safeChart")
+CANVAS.setAttribute('height', `${DEFAULTS.yAxis.lines * DEFAULTS.rowHeight + DEFAULTS.yAxis.offset}px`)
+CANVAS.setAttribute('width', `500px`)
+var ctx = CANVAS.getContext("2d")
+// included in html
+// bigdata = bigdata[0]
+// console.log(bigdata)
+
+
 // draw some labels
 drawAxisLables()
+// draw some lines
+drawPlotLines()
 // draw the rest of the chart
 drawTempChart()
+
+// function timeConverter(UNIX_timestamp){
+//   var a = new Date(UNIX_timestamp * 1000);
+//   var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+//   var year = a.getFullYear();
+//   var month = months[a.getMonth()];
+//   var date = a.getDate();
+//   var hour = a.getHours();
+//   var min = a.getMinutes();
+//   var sec = a.getSeconds();
+//   var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+//   return time;
+// }
+// console.log(timeConverter(0));
