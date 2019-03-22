c9e20e560375d30ca802ad2dfd71ff9aa620287c
IDIDIR <IDIDIR@aicc.online>
2019-03-22 19:31:31 +0600

    added minimap with scrolling

--- js/safeCore.js
+++ js/safeCore.js
@@ -7,8 +7,11 @@
 "use strict"
 
 // project pride
-var rev =(y)=> (CANVAS.height-y*DEFAULTS.scale)
+var rev =(y)=> (CANVAS.height+DEFAULTS.xAxis.margin-y*DEFAULTS.scale*(CANVAS.height/(DEFAULTS.yAxis.max*DEFAULTS.scale)))
 var t2d =(t)=> ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'][(new Date(t)).getMonth()]+' '+(new Date(t)).getDate()
+
+var CANVAS,MAPSEL,MAPLAY
+var ctx,mtx
 
 var theLines = Array()
 var theAxes = Object()
@@ -36,15 +39,19 @@ const DEFAULTS = {
 	yAxis: {
 		// offset: 0,	// dynamic
 		margin: 30,
-		padding: 0,
+		padding: 10,
 		labels: 6,
 		color: '#ADADADAD',
 		lines: 6,
 		step: 60,
+		max: 0
 	},
 }
 
 var parseBadStruct = function() {
+	// included in html
+	bigdata = bigdata[0]
+	console.log(bigdata)
 	let keys = Array()
 	// bazat is a connecting link in any incomprehensible situation
 	bigdata.columns.map((bazat)=>keys.push(bazat[0]))
@@ -62,7 +69,29 @@ var parseBadStruct = function() {
 	})
 }
 
-var drawAxisLables = function() {	
+var initSelectors = function() {
+	CANVAS = document.getElementById("safeChart")
+	MAPSEL = document.getElementById("mapSelector")
+	MAPLAY = document.getElementById("mapLayout")
+	CANVAS.setAttribute('height', `${DEFAULTS.yAxis.lines * DEFAULTS.rowHeight + DEFAULTS.yAxis.margin}px`)
+	CANVAS.setAttribute('width', `1000px`)
+	MAPSEL.style.width = `${DEFAULTS.rowWidth * theAxes.data.length / CANVAS.width}px`
+	ctx = CANVAS.getContext("2d")
+	mtx = MAPLAY.getContext("2d")
+}
+
+var drawAxisLables = function() {
+	Object.entries(theLines).forEach(([key, line])=>{
+		// so that the schedule does not go beyond the workspace
+		const additionalPadding = 10/DEFAULTS.scale
+		let maxValue = Math.max(...line.data)
+		if(maxValue>DEFAULTS.yAxis.max) {
+			// find a close multiple of ten after multiplying by scale
+			DEFAULTS.yAxis.max=Math.ceil(maxValue)+additionalPadding
+		}
+		// console.log(DEFAULTS.yAxis.max)
+	})
+
 	let rowCount = DEFAULTS.yAxis.labels
 	let colCount = theAxes.data.length
 	let rowHeight = DEFAULTS.rowHeight
@@ -75,12 +104,14 @@ var drawAxisLables = function() {
 	ctx.beginPath()
 	// yAxis labels (static)
 	for (var i = 0; i < rowCount; i++) {
-		ctx.fillText(rowHeight * i, 0, CANVAS.height - rowHeight * i)
+		ctx.fillText(parseInt(DEFAULTS.yAxis.max * DEFAULTS.scale / rowCount * i), 0, CANVAS.height - rowHeight * i - DEFAULTS.yAxis.padding)
+		// ctx.fillText(rowHeight * i, 0, CANVAS.height - rowHeight * i - DEFAULTS.yAxis.padding)
 	}
 	ctx.stroke()
 
 	ctx.textAlign = 'center'
-	ctx.translate(DEFAULTS.xAxis.margin, DEFAULTS.yAxis.margin)
+	ctx.translate(0, DEFAULTS.yAxis.margin)
+	// ctx.translate(DEFAULTS.xAxis.margin, DEFAULTS.yAxis.margin)
 	ctx.beginPath()
 	// xAxis labels (dynamic)
 	for (var i = 0; i < colCount; i++) {
@@ -92,6 +123,7 @@ var drawAxisLables = function() {
 }
 
 var drawPlotLines = function() {
+	let canvasFullWidth = DEFAULTS.rowWidth*theAxes.data.length	// del
 	let rowCount = DEFAULTS.yAxis.labels
 	let colCount = theAxes.data.length
 	let rowHeight = DEFAULTS.rowHeight
@@ -102,7 +134,8 @@ var drawPlotLines = function() {
 	// yAxis lines
 	for (var i = 0; i < rowCount; i++) {
 		ctx.moveTo(0, CANVAS.height - rowHeight * i)								// 1
-		ctx.lineTo(CANVAS.width, CANVAS.height - rowHeight * i)			// 2
+		// ctx.lineTo(CANVAS.width, CANVAS.height - rowHeight * i)			// 2
+		ctx.lineTo(canvasFullWidth, CANVAS.height - rowHeight * i)			// 2
 	}
 	ctx.stroke()
 	ctx.beginPath()
@@ -116,7 +149,7 @@ var drawPlotLines = function() {
 	ctx.stroke()
 }
 
-var drawEtcChart = function() {
+var drawChartLines = function() {
 	ctx.lineWidth = 2.5
 	ctx.lineJoin = DEFAULTS.lineJoin
 	Object.entries(theLines).forEach(([key,line]) => {
@@ -140,54 +173,67 @@ var drawEtcChart = function() {
 	})
 }
 
+var pluginScrolling = function() {
+
+	// selector moves around the map only xAxis
+	let innerLeftOffset = 0
+	let lastClientX = MAPSEL.getBoundingClientRect().x
+	MAPSEL.addEventListener('mousedown', mouseDown, false)
+	window.addEventListener('mouseup', mouseUp, false)
+	
+	function mouseUp(e) {
+		let canvasFullWidth = DEFAULTS.rowWidth*theAxes.data.length
+		const SELECTOR = MAPSEL.getBoundingClientRect()
+		// render after mouseup (well, at least here the optimization was delivered)
+		ctx.translate( -(SELECTOR.x - lastClientX) * ( (canvasFullWidth - CANVAS.width) / (400 - SELECTOR.width) ), DEFAULTS.yAxis.margin)
+		// ctx.clearRect(0, 0, CANVAS.width, CANVAS.height)
+		ctx.clearRect(0, 0, canvasFullWidth, CANVAS.height)
+		lastClientX = SELECTOR.x
+		drawAxisLables()
+		drawPlotLines()
+		drawChartLines()
+		window.removeEventListener('mousemove', move, true)
+	}
+	function mouseDown(e) {
+		innerLeftOffset = MAPSEL.offsetLeft - e.clientX
+		window.addEventListener('mousemove', move, true)
+	}
+	function move(e) {
+		const SELECTOR = MAPSEL.getBoundingClientRect()
+		const LAYOUT = MAPLAY.getBoundingClientRect()
+		let offsetByParent = e.clientX + innerLeftOffset
+		if(offsetByParent>=0&&offsetByParent<=(LAYOUT.width-SELECTOR.width)) MAPSEL.style.left = e.clientX + innerLeftOffset + 'px'
+	}
+}
+
+var drawMapLines = function() {
+	mtx.lineWidth = 1.5
+	mtx.lineJoin = DEFAULTS.lineJoin
+	Object.entries(theLines).forEach(([key,line]) => {
+		
+		mtx.strokeStyle = line.color
+		mtx.beginPath()
+		line.data.forEach((dot,i) => {
+			let y = MAPLAY.height-dot*DEFAULTS.scale*(MAPLAY.height/(DEFAULTS.yAxis.max*DEFAULTS.scale));
+			let rowWidth = MAPLAY.width/theAxes.data.length;
+			(!i)?mtx.moveTo(rowWidth*i, y):mtx.lineTo(rowWidth*i, y);
+		})
+		mtx.stroke()
+		mtx.fillStyle = 'rgba(137, 162, 165, 0.05)'
+		mtx.fillRect(0, 0, 400, 60);
+
+	})
+}
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-const CANVAS = document.getElementById("safeChart")
-const MAPSEL = document.getElementById("mapSelector")
-const MAPLAY = document.getElementById("mapLayout")
-CANVAS.setAttribute('height', `${DEFAULTS.yAxis.lines * DEFAULTS.rowHeight + DEFAULTS.yAxis.margin}px`)
-CANVAS.setAttribute('width', `1000px`)
-var ctx = CANVAS.getContext("2d")
-// included in html
-bigdata = bigdata[0]
-console.log(bigdata)
-
-
-// here it is necessary to keep silent
 parseBadStruct()
-// draw some labels
+initSelectors()
+
+pluginScrolling()
+// pluginScaling()
+
 drawAxisLables()
-// draw some lines
 drawPlotLines()
-// draw the rest of the chart
-drawEtcChart()
-
-
-// selector moves around the map only xAxis
-let innerLeftOffset = 0
-let lastClientX = MAPSEL.getBoundingClientRect().x
-MAPSEL.addEventListener('mousedown', mouseDown, false)
-window.addEventListener('mouseup', mouseUp, false)
-
-function mouseUp(e) {
-	let canvasFullWidth = DEFAULTS.rowWidth*theAxes.data.length
-	const SELECTOR = MAPSEL.getBoundingClientRect()
-	// render after mouseup (well, at least here the optimization was delivered)
-	// ctx.clearRect(0, 0, CANVAS.width, CANVAS.height)
-	ctx.clearRect(0, 0, canvasFullWidth, CANVAS.height)
-	ctx.translate( -(SELECTOR.x - lastClientX) * ( (canvasFullWidth - CANVAS.width) / (400 - SELECTOR.width) ), 0)
-	lastClientX = SELECTOR.x
-	drawEtcChart()
-	window.removeEventListener('mousemove', move, true)
-}
-function mouseDown(e) {
-	innerLeftOffset = MAPSEL.offsetLeft - e.clientX
-	window.addEventListener('mousemove', move, true)
-}
-function move(e) {
-	const SELECTOR = MAPSEL.getBoundingClientRect()
-	const LAYOUT = MAPLAY.getBoundingClientRect()
-	let offsetByParent = e.clientX + innerLeftOffset
-	if(offsetByParent>=0&&offsetByParent<=(LAYOUT.width-SELECTOR.width)) MAPSEL.style.left = e.clientX + innerLeftOffset + 'px'
-}
+drawChartLines()
+drawMapLines()
